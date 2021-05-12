bc0b4b1facc830e2e0276618b8d31cf66a13a504
olive-green <nareshbhairathore@gmail.com>
2021-05-13 02:47:59 +0530

    adding cookies , profile getting files

--- app.js
+++ app.js
@@ -1,14 +1,37 @@
 const express=require("express");
 const passport = require("passport");
+const mongoose=require("mongoose");
 //import router
 const authRoutes=require("./routes/auth-routes");
 const app=express()
+//import dbURL
+const keys=require("./config/keys");
+const cookieSession=require("cookie-session");
+
+
+// set up view engine
+app.set("view engine","ejs");
+
+//setting cookies
+app.use(cookieSession({
+    maxAge:24*60*60*1000,
+    keys:[keys.session.key] //import encrypted key from keys
+}))
+
+//initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+//db connection
+mongoose.connect(keys.mongodb.dbURL,{useNewUrlParser:true,useUnifiedTopology:true})
+.then(()=> console.log("database connected successfully"))
+.catch(err=>console.log(err));
+
 
 //require passport-setup to run google strategy
 const passportSetup=require("./config/passport-setup");
 
-// set up view engine
-app.set("view engine","ejs");
 
 //routes middleware 
 app.use("/auth",authRoutes);
--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,7 +1,24 @@
 const passport=require("passport");
 const keys=require("./keys");
+const User=require("../models/user-model");
 const GoogleStrategy=require("passport-google-oauth20");
 const HttpsProxyAgent = require('https-proxy-agent');
+
+//create cookies
+passport.serializeUser((user,done)=>{
+    //now here we call done() function to move the user to next state 
+    //it simply grabs the user from done() function and then stores into cookie
+    done(null,user.id); //here id is from mongo collection
+})
+
+passport.deserializeUser((id,done)=>{
+    //now we find that user with help of id and mathces with our cookie
+    User.findById(id).then(user=>{
+        done(null,user);
+    })
+})
+
+
 //using a google strategy`
 passport.use(new GoogleStrategy({
     //options for google strategy
@@ -10,8 +27,32 @@ passport.use(new GoogleStrategy({
     clientSecret: keys.google.clientSecret
 },(accessToken,refreshToken,profile,done)=>{ //accessToken is a token given by google to us , refreshToken is a token which regenerate the accessToken which gets expired always after some time and done is a function which will run after this callback function is completed 
     //passport callback functions
-    console.log("passport callback function fired");
-    console.log(profile);
+    //now inside this callback function 
+    // we check whether the user profile is stored in our database or not if not then we add this profile to our database and if yes then we just retreive the profile info from our database
+    // console.log("passport callback function fired");
+    // console.log(profile);
+
+//now check whether the user is already exists or not
+User.findOne({googleId:profile.id})
+.then((currentUser)=>{
+    if(currentUser){
+        //this user is already exists
+        done(null,currentUser);
+    }
+    else
+    {
+        
+        //saving profile to database
+        new User({
+            username:profile.displayName,
+            googleId:profile.id
+        }).save().then((newUser)=>{
+            done(null,newUser);
+            console.log("new user created:",newUser);
+        })
+    }
+})
+
 })
 )
 
